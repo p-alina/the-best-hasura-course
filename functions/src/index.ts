5d90f47970a3ac0451ccb05f348f5914584bf1db
Alina Platon <alinna.platon@gmail.com>
2021-07-07 18:11:14 +0300

    Create remoteSchema

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,13 @@ import * as functions from "firebase-functions";
 import {notifyAboutComment01Handler} from "./notifyAboutComment01";
 import {createUserHandler} from "./createUser";
 import {getProfileHandler} from "./getProfile";
+import initApolloServer from "./remoteSchema";
 
 export const notifyAboutComment01 = functions.https.onRequest(
     notifyAboutComment01Handler
 );
 export const createUser = functions.https.onRequest(createUserHandler);
 export const getProfile = functions.https.onRequest(getProfileHandler);
+export const userProfile = functions.https.onRequest(
+    initApolloServer.createHandler()
+);
